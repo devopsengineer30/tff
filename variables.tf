5ff77b8baf2e1e4b447d1d54acabf88c40db5a67
devopsengineer30 <47628875+devopsengineer30@users.noreply.github.com>
2021-08-26 18:52:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "access_key" {
-  default = "AKIA5KOY4NEOEZ4VCKGU"
+  default = "AKIATFXW3VG5XQQV7UVE"
   }
 variable "secret_key" {
-  default = "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"
+  default = "CpH70a+islxqZdewXve7FITeLdz/l+sD7FtjOmPu"
   }
 variable "region" {
-  default = "ap-south-1"
+  default = "us-east-2"
   }
